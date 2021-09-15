cf2c82349a43035db97bb4fe116cdb50f34d58a0
abdelghani-khalidi <51909290+abdelghani-khalidi@users.noreply.github.com>
2021-09-15 17:50:02 +0200

    Update main.py

--- cors_open/controllers/main.py
+++ cors_open/controllers/main.py
@@ -1,14 +1,49 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
+import babel.messages.pofile
+import base64
+import copy
+import datetime
+import functools
+import glob
+import hashlib
+import io
+import itertools
+import jinja2
+import json
 import logging
-import werkzeug
+import operator
+import os
+import re
+import sys
+import tempfile
+import time
 
-from odoo import http, _
-from odoo.addons.auth_signup.models.res_users import SignupError
-from odoo.addons.web.controllers.main import ensure_db, Home
-from odoo.addons.base_setup.controllers.main import BaseSetup
-from odoo.exceptions import UserError
-from odoo.http import request
+import werkzeug
+import werkzeug.exceptions
+import werkzeug.utils
+import werkzeug.wrappers
+import werkzeug.wsgi
+from collections import OrderedDict, defaultdict, Counter
+from werkzeug.urls import url_decode, iri_to_uri
+from lxml import etree
+import unicodedata
+
+
+import odoo
+import odoo.modules.registry
+from odoo.api import call_kw, Environment
+from odoo.modules import get_module_path, get_resource_path
+from odoo.tools import image_process, topological_sort, html_escape, pycompat, ustr, apply_inheritance_specs, lazy_property, float_repr
+from odoo.tools.mimetypes import guess_mimetype
+from odoo.tools.translate import _
+from odoo.tools.misc import str2bool, xlsxwriter, file_open
+from odoo.tools.safe_eval import safe_eval
+from odoo import http, tools
+from odoo.http import content_disposition, dispatch_rpc, request, serialize_exception as _serialize_exception, Response
+from odoo.exceptions import AccessError, UserError, AccessDenied
+from odoo.models import check_method_name
+from odoo.service import db, security
 
 _logger = logging.getLogger(__name__)
 
