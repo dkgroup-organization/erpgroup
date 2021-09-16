820c04de43625e6861880c64662e5640aabeddda
abdelghani-khalidi <51909290+abdelghani-khalidi@users.noreply.github.com>
2021-09-16 11:30:14 +0200

    Update main.py

--- cors_open/controllers/main.py
+++ cors_open/controllers/main.py
@@ -877,47 +877,47 @@ class Home(http.Controller):
     def _login_redirect(self, uid, redirect=None):
         return redirect if redirect else '/web'
 
-    @http.route('/web/login', type='http', auth="none", cors='*')
-    def web_login(self, redirect=None, **kw):
-        ensure_db()
-        request.params['login_success'] = False
-        if request.httprequest.method == 'GET' and redirect and request.session.uid:
-            return http.redirect_with_hash(redirect)
+#     @http.route('/web/login', type='http', auth="none", cors='*')
+#     def web_login(self, redirect=None, **kw):
+#         ensure_db()
+#         request.params['login_success'] = False
+#         if request.httprequest.method == 'GET' and redirect and request.session.uid:
+#             return http.redirect_with_hash(redirect)
 
-        if not request.uid:
-            request.uid = odoo.SUPERUSER_ID
+#         if not request.uid:
+#             request.uid = odoo.SUPERUSER_ID
 
-        values = request.params.copy()
-        try:
-            values['databases'] = http.db_list()
-        except odoo.exceptions.AccessDenied:
-            values['databases'] = None
+#         values = request.params.copy()
+#         try:
+#             values['databases'] = http.db_list()
+#         except odoo.exceptions.AccessDenied:
+#             values['databases'] = None
 
-        if request.httprequest.method == 'POST':
-            old_uid = request.uid
-            try:
-                uid = request.session.authenticate(request.session.db, request.params['login'], request.params['password'])
-                request.params['login_success'] = True
-                return http.redirect_with_hash(self._login_redirect(uid, redirect=redirect))
-            except odoo.exceptions.AccessDenied as e:
-                request.uid = old_uid
-                if e.args == odoo.exceptions.AccessDenied().args:
-                    values['error'] = _("Wrong login/password")
-                else:
-                    values['error'] = e.args[0]
-        else:
-            if 'error' in request.params and request.params.get('error') == 'access':
-                values['error'] = _('Only employee can access this database. Please contact the administrator.')
+#         if request.httprequest.method == 'POST':
+#             old_uid = request.uid
+#             try:
+#                 uid = request.session.authenticate(request.session.db, request.params['login'], request.params['password'])
+#                 request.params['login_success'] = True
+#                 return http.redirect_with_hash(self._login_redirect(uid, redirect=redirect))
+#             except odoo.exceptions.AccessDenied as e:
+#                 request.uid = old_uid
+#                 if e.args == odoo.exceptions.AccessDenied().args:
+#                     values['error'] = _("Wrong login/password")
+#                 else:
+#                     values['error'] = e.args[0]
+#         else:
+#             if 'error' in request.params and request.params.get('error') == 'access':
+#                 values['error'] = _('Only employee can access this database. Please contact the administrator.')
 
-        if 'login' not in values and request.session.get('auth_login'):
-            values['login'] = request.session.get('auth_login')
+#         if 'login' not in values and request.session.get('auth_login'):
+#             values['login'] = request.session.get('auth_login')
 
-        if not odoo.tools.config['list_db']:
-            values['disable_database_manager'] = True
+#         if not odoo.tools.config['list_db']:
+#             values['disable_database_manager'] = True
 
-        response = request.render('web.login', values)
-        response.headers['X-Frame-Options'] = 'DENY'
-        return response
+#         response = request.render('web.login', values)
+#         response.headers['X-Frame-Options'] = 'DENY'
+#         return response
 
     @http.route('/web/become', type='http', auth='user', sitemap=False)
     def switch_to_admin(self):
