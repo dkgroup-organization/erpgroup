7f2a58899e1fe4c483bb9790ebf2f98598d181a7
Asmaa-khallout <asmaa.khallout@dkgroup.fr>
2021-10-18 10:30:52 +0100

    update amount move

---
+++ custom_setting/__manifest__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+# Part of BrowseInfo. See LICENSE file for full copyright and licensing details.
+
+{
+    'name' : "Custom Setting Dk group",
+    'version' : "14.0.0.0",
+    'description' : """
+        
+        
+
+     """,
+    'author' : "Asmaa Khallout",
+    'website'  : "https://dkgroup.fr",
+    'depends': ['account'],
+    # 'external_dependencies': {
+    #     'python': ['zxcvbn'],
+    # },
+    'data'     : ['views/setting_setting.xml'],
+    'installable' : True,
+    'application' :  True,
+    
+}
---
+++ custom_setting/models/setting_setting.py
@@ -0,0 +1,24 @@
+# Copyright 2016-2017 Akretion (http://www.akretion.com)
+# Copyright 2016-2017 Camptocamp (http://www.camptocamp.com/)
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+from odoo import api, fields, models, _
+from odoo.exceptions import RedirectWarning, UserError, ValidationError, AccessError
+import logging
+_logger = logging.getLogger(__name__)
+
+class setting_setting_aydoo(models.TransientModel):
+    _name = 'setting.setting'
+    _description = 'Setting'
+
+    def update_moves_amount(self):
+        moves = self.env['account.move'].search([('type','in',('out_invoice','in_invoice')),('state','in',('draft','cancel'))])
+        _logger.info("nbre de factures %s" % (len(moves)))
+        i=0
+        for p in moves:
+            i+=1
+            _logger.info("Eteration %s" % (i))
+            _logger.info("N° factures est  %s est %s" % (p.name,p.id))
+            p._compute_amount()
+
+
