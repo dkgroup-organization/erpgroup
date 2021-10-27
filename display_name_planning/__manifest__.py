3d4123a0d82f53b5f5baf57a080b9d829ee7978e
abdelghani-khalidi <51909290+abdelghani-khalidi@users.noreply.github.com>
2021-10-27 10:10:13 +0100

    Add files via upload

---
+++ display_name_planning/__manifest__.py
@@ -0,0 +1,15 @@
+# Copyright 2016-2017 Akretion (http://www.akretion.com)
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+{
+    'name': 'display_name_planning',
+    'version': '13.0.1.0.0',
+    'summary': 'Technical module to change display name in planning '
+    ' State',
+    'author': 'Abdelghani KHALIDI',
+    'website': 'https://DKGROUP.FR',
+    'license': 'AGPL-3',
+    'category': 'Generic Modules',
+    'depends': ['product','planning'],
+    'installable': True,
+}
---
+++ display_name_planning/models/state.py
@@ -0,0 +1,23 @@
+# Copyright 2016-2017 Akretion (http://www.akretion.com)
+# Copyright 2016-2017 Camptocamp (http://www.camptocamp.com/)
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+import xml.etree.ElementTree as xee
+from odoo import api, exceptions, fields, models, _
+from datetime import datetime
+
+class planning_display(models.Model):
+    _inherit = "planning.slot"
+    
+    display_name = fields.Char(
+        string='display name',
+        compute='_compute_display')
+    
+    
+   
+
+
+    @api.depends('employee_id','project_id','role_id')
+    def _compute_display(self):
+        for var in self:
+             var.display_name=  str(var.employee_id.name) +"\n #PROJET: " +str(var.project_id.name) + "\n  #TÄCHE: " + str(var.task_id.name)
