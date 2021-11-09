2e7e40542292232f5f7999d0352d8b3ed4677b6f
aminesoulaymani <s.amine.impactnet@gmail.com>
2021-11-09 13:51:17 +0100

    commit asma

--- dk_customs_additionnal/models/models.py
+++ dk_customs_additionnal/models/models.py
@@ -6,20 +6,22 @@ from odoo import models, fields, api
 class dk_customs_additionnal(models.Model):
     _inherit = 'res.partner'
 
-    third_account_customer = fields.Many2one('account.account', string='compte tiers client')
-    third_account_supplier = fields.Many2one('account.account', string='compte tiers fournisseur')
+    third_account_customer = fields.Char(string='compte tiers client')
+    third_account_supplier = fields.Char(string='compte tiers fournisseur')
 
 
 class dk_customs_additionnal(models.Model):
     _inherit = 'account.move.line'
 #
-    compte_tiers  =fields.Many2one('account.account', string='compte tiers client',compute="get_compte_tiers")
-#
+    compte_tiers  =fields.Char(string='compte tiers client',compute="get_compte_tiers")
 #
     def get_compte_tiers(self):
-        if self.account_id.user_type_id.name == "Recevable":
+        customers_labels = ("Recevable", 'Revenus',)
+        supplier_labels = ("Payable",)
+
+        if self.account_id.user_type_id.name in customers_labels:
             self.compte_tiers = self.partner_id.third_account_customer
-        elif self.account_id.user_type_id.name == "Payable":
+        elif self.account_id.user_type_id.name in supplier_labels:
             self.compte_tiers = self.partner_id.third_account_supplier
         else:
             self.compte_tiers = None
