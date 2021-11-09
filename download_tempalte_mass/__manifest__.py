42352df0a1dccf1256863b0495d79fbe3185b60f
abdelghani-khalidi <51909290+abdelghani-khalidi@users.noreply.github.com>
2021-11-09 11:20:18 +0100

    Add files via upload

---
+++ download_tempalte_mass/__manifest__.py
@@ -0,0 +1,15 @@
+# Copyright 2016-2017 Akretion (http://www.akretion.com)
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+{
+    'name': 'download_tempalte_mass',
+    'version': '13.0.1.0.0',
+    'summary': 'Technical module to download template_mass_mailling '
+    ' State',
+    'author': 'Abdelghani KHALIDI',
+    'website': 'https://DKGROUP.FR',
+    'license': 'AGPL-3',
+    'category': 'Generic Modules',
+    'depends': ['mass_mailing'],
+    'installable': True,
+}
---
+++ download_tempalte_mass/models/state.py
@@ -0,0 +1,49 @@
+# Copyright 2016-2017 Akretion (http://www.akretion.com)
+# Copyright 2016-2017 Camptocamp (http://www.camptocamp.com/)
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+import xml.etree.ElementTree as xee
+from odoo import api, exceptions, fields, models, _
+from datetime import datetime
+import base64
+
+
+
+class MassMailing2(models.Model):
+    """ MassMailing models a wave of emails for a mass mailign campaign.
+    A mass mailing is an occurence of sending emails. """
+  _inherit = 'mailing.mailing'
+
+
+  def download template(self):
+    f_read = open('test.html','w')
+    message = self.body_arch
+    f_read.write(message)
+    file_data = f_read.read()
+    values = {
+            'name': "Name of text file.txt",
+            'datas_fname': 'print_file_name.txt',
+            'res_model': 'ir.ui.view',
+            'res_id': False,
+            'type': 'binary',
+            'public': True,
+            'datas': file_data.encode('utf8').encode('base64'),
+        }
+    attachment_id = self.env['ir.attachment'].sudo().create(values)
+    #Prepare your download URL
+    download_url = '/web/content/' + str(attachment_id.id) + '?download=True'
+    base_url = self.env['ir.config_parameter'].get_param('web.base.url')
+    
+    return {
+        "type": "ir.actions.act_url",
+        "url": str(base_url)  +  str(download_url),
+        "target": "new",
+     }
+
+
+
+
+
+
+
+    body_arch
