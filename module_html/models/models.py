6fdc4919535081d802dccd6de53104ef8f9e7e75
aichadkgoup <90616405+aichadkgoup@users.noreply.github.com>
2021-11-16 10:28:30 +0100

    Update models.py

--- module_html/models/models.py
+++ module_html/models/models.py
@@ -8,4 +8,4 @@ _logger = logging.getLogger(__name__)
 
 class cate(models.Model):
     _inherit = "sale.order"
-    name = fields.Text(string='Description', required=True)
+    name = fields.Html(string='Description', required=True)
