c443a394ff88b03b74089eedd2406f00d89b01fc
aichadkgoup <90616405+aichadkgoup@users.noreply.github.com>
2021-11-26 10:10:43 +0100

    Update etiquite.py

--- etiquite/models/etiquite.py
+++ etiquite/models/etiquite.py
@@ -10,3 +10,6 @@ class etiquite(models.Model):
     _inherit = 'project.task'
 
     tag_ids = fields.Many2many('project.tags', string='testtt',required=True)
+    project_id = fields.Many2one('project.project', string='Project yyyy',
+        compute='_compute_project_id', store=True, readonly=True,
+        index=True, tracking=True, check_company=True, change_default=True)
