e3d69d7f1094ca5de2ed23a3bff214ed3e11b52a
aichadkgoup <90616405+aichadkgoup@users.noreply.github.com>
2021-11-26 16:31:08 +0100

    Add files via upload

---
+++ delevrie/__manifest__.py
@@ -0,0 +1,20 @@
+{
+    'name': 'deliverie',
+    'summary': "",
+    'description': """
+
+    """,
+    'sequence': '1',
+    'version': '14.0.1.1.1',
+    'author': '',
+    'maintainer': 'BBA',
+    'category': 'Custom',
+    'depends': ['project'],
+
+    # 'data': [
+    #     'views/etiquite.xml',
+    # ],
+
+    'application': True,
+    'installable': True,
+}
---
+++ delevrie/models/deliveri.py
@@ -0,0 +1,25 @@
+from odoo import api, fields, models, _
+
+class projectt(models.Model):
+    _inherit = 'purchase.order'
+
+
+
+
+    #achat_test = fields.Many2one('purchase.order', string='Achats', required=True)
+
+    # @api.onchange('achat')
+    # def _getfilter(self):
+    #     data = self.env['project.project'].browse(self._context.get('active_ids', []))
+    #     return {'domain': {'achat': [('id', 'in', data.achats.ids)]}}
+
+    def action_delier_achat(self):
+        data = self.env['project.project'].browse(self._context.get('active_ids', []))
+        for m in data:
+            self.projet = False
+            m = [(3, self.id)]
+
+
+
+
+
