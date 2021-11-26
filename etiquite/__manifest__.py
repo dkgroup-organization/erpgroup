dba85d3dd294147b2504ab45735dda7a7c4fe6c1
aichadkgoup <90616405+aichadkgoup@users.noreply.github.com>
2021-11-26 09:42:17 +0100

    Add files via upload

---
+++ etiquite/__manifest__.py
@@ -0,0 +1,20 @@
+{
+    'name': 'etiquite',
+    'summary': "",
+    'description': """
+
+    """,
+    'sequence': '1',
+    'version': '14.0.1.1.1',
+    'author': '',
+    'maintainer': 'BBA',
+    'category': 'Custom',
+    'depends': ['project'],
+
+    'data': [
+        #  'security/rule.xml',
+    ],
+
+    'application': True,
+    'installable': True,
+}
