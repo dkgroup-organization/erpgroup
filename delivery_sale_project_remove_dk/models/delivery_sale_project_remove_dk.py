606aa2a89b5271aca7c392087391dca6ac8242d1
aichadkgoup <90616405+aichadkgoup@users.noreply.github.com>
2021-11-26 17:30:15 +0100

    Update delivery_sale_project_remove_dk.py

--- delivery_sale_project_remove_dk/models/delivery_sale_project_remove_dk.py
+++ delivery_sale_project_remove_dk/models/delivery_sale_project_remove_dk.py
@@ -1,6 +1,12 @@
 from odoo import api, fields, models, _
 from odoo.exceptions import RedirectWarning, UserError, ValidationError, AccessError
 
+
+
+import logging,pprint
+
+_logger = logging.getLogger(__name__)
+
 class delivery_sale_project_remove_dk_inherited(models.Model):
     _inherit = 'purchase.order'
 
@@ -15,6 +21,8 @@ class delivery_sale_project_remove_dk_inherited(models.Model):
     #     return {'domain': {'achat': [('id', 'in', data.achats.ids)]}}
 
     def action_delier_achat(self):
+        data = self.env['project.project'].browse(self._context.get('active_ids', []))
+        _logger.info("Eteration %s",data)
         self.projet = False
         data = self.env['project.project'].browse(self._context.get('active_ids', []))
         for m in data:
