c3016e6dfba87a3679a5da49a16faeb29f160071
aichadkgoup <90616405+aichadkgoup@users.noreply.github.com>
2021-11-26 10:00:55 +0100

    Update etiquite.py

--- etiquite/models/etiquite.py
+++ etiquite/models/etiquite.py
@@ -9,5 +9,4 @@ from odoo import api, fields, models, tools, SUPERUSER_ID, _
 class etiquite(models.Model):
     _inherit = 'project.task'
 
-    sequence = fields.Integer(string='Sequence', index=True, default=10,
-        help="Gives the sequence order when displaying a list of tasks.")
+    tag_ids = fields.Many2many('project.tags', string='Tags')
