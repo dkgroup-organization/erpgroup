890548a03eb42ef476f17ae278f3d5eb2ffcfa1d
aichadkgoup <90616405+aichadkgoup@users.noreply.github.com>
2021-11-26 09:49:39 +0100

    Update etiquite.py

--- etiquite/models/etiquite.py
+++ etiquite/models/etiquite.py
@@ -9,4 +9,4 @@ from odoo import api, exceptions, fields, models, _
 class acompte(models.Model):
     _inherit = 'project.task'
 
-    tag_ids = fields.Many2many('project.tags', string='test',domain="['|', ('x_company_id', '=', False), ('x_company_id', '=', company_id)]")
+    tag_ids = fields.Many2many('project.tags', string='test',domain="['|', ('company_id', '=', False), ('company_id', '=', company_id)]")
