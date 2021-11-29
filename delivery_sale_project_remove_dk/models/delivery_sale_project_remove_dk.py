768e5a813ed386b3886ced1e5b7d4a5677b39f89
aichadkgoup <90616405+aichadkgoup@users.noreply.github.com>
2021-11-29 15:03:24 +0100

    Update delivery_sale_project_remove_dk.py

--- delivery_sale_project_remove_dk/models/delivery_sale_project_remove_dk.py
+++ delivery_sale_project_remove_dk/models/delivery_sale_project_remove_dk.py
@@ -7,17 +7,12 @@ _logger = logging.getLogger(__name__)
 class delivery_sale_project_remove_dk_inherited(models.Model):
     _inherit = 'purchase.order'
 
-    # achat_test = fields.Many2one('purchase.order', string='Achats', required=True)
-
-    # @api.onchange('achat')
-    # def _getfilter(self):
-    #     data = self.env['project.project'].browse(self._context.get('active_ids', []))
-    #     return {'domain': {'achat': [('id', 'in', data.achats.ids)]}}
 
     def action_delier_achat(self):
         data = self.env['purchase.order'].search([("id", "=", self.id)])
+        
+        self.projet.achats = [(3, self.id)]
         self.projet = False
-        data = [(3, self.id)]
 
 class delivery_facture_project_remove_dk_inherited(models.Model):
     _inherit = 'account.move'
