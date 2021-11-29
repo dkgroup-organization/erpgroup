8a8c7f5d8fb707327509966f0392640bb89899f6
aichadkgoup <90616405+aichadkgoup@users.noreply.github.com>
2021-11-29 14:38:46 +0100

    Update delivery_sale_project_remove_dk.py

--- delivery_sale_project_remove_dk/models/delivery_sale_project_remove_dk.py
+++ delivery_sale_project_remove_dk/models/delivery_sale_project_remove_dk.py
@@ -1,6 +1,8 @@
 from odoo import api, fields, models, _
 from odoo.exceptions import RedirectWarning, UserError, ValidationError, AccessError
 
+import logging
+_logger = logging.getLogger(__name__)
 
 class delivery_sale_project_remove_dk_inherited(models.Model):
     _inherit = 'purchase.order'
@@ -23,7 +25,9 @@ class delivery_facture_project_remove_dk_inherited(models.Model):
     def action_delier_account(self):
         data = self.env['account.move'].search([("id", "=", self.id)])
         self.projet = False
+        _logger.info("testttttttttttttttttt %s" % data)
         data = [(3, self.id)]
+        _logger.info("testttttttttttttttttt2 %s" % data)
 
 class delivery_vende_project_remove_dk_inherited(models.Model):
     _inherit = 'sale.order'
