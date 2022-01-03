695f8ba2714ce0ce8cf556d97ea038fc2a85eaa0
aichadkgoup <90616405+aichadkgoup@users.noreply.github.com>
2022-01-03 17:46:43 +0100

    Update ajouter_contact.py

--- ajouter_auto_adress/models/ajouter_contact.py
+++ ajouter_auto_adress/models/ajouter_contact.py
@@ -28,7 +28,7 @@ class facture(models.Model):
         var = super(facture, self).create(values)
         child_ids = var.partner_id.child_ids
         item_ids = [line_ for line_ in child_ids if line_.type == "invoice"]
-        var.x_contact = item_ids[0]
+        var.x_contact = item_ids[1]
         return var
 
     @api.onchange('partner_id')
