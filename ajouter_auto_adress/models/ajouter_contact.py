cacef942f7108d8d2cb919d403d68736e639ce30
aichadkgoup <90616405+aichadkgoup@users.noreply.github.com>
2022-01-04 10:21:42 +0100

    Update ajouter_contact.py

--- ajouter_auto_adress/models/ajouter_contact.py
+++ ajouter_auto_adress/models/ajouter_contact.py
@@ -33,7 +33,8 @@ class facture(models.Model):
             item_ids = [line_ for line_ in child_ids if line_.type == "invoice"]
   
             #contact = self.env['account.move'].browse(x_contact)
-            _logger.info('yyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyy %s', contact)
+            _logger.info('yyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyy %s', var.x_contact)
+            _logger.info('yyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyy3 %s', item_ids[0])
             var.x_contact = item_ids[0].id
          
       
