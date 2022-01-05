e77543135a98c89f5d69cf96d8e7ffe861b29b18
aichadkgoup <90616405+aichadkgoup@users.noreply.github.com>
2022-01-05 10:08:06 +0100

    Update add_company.py

--- company_par_default_contact/models/add_company.py
+++ company_par_default_contact/models/add_company.py
@@ -5,8 +5,8 @@ from odoo import fields, models, api, _
 
 
 
-class account(models.Model):
-    _inherit = 'account.move'
+class partner(models.Model):
+    _inherit = 'res.partner'
 
     company_id = fields.Many2one('res.company', required=True, readonly=True, default=lambda self: self.env.company)
 
