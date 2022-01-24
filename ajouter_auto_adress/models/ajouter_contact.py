8ea20e328e4a9adf5f7b411dad1b4aef22da4fd4
aichadkgoup <90616405+aichadkgoup@users.noreply.github.com>
2022-01-24 17:32:34 +0100

    Update ajouter_contact.py

--- ajouter_auto_adress/models/ajouter_contact.py
+++ ajouter_auto_adress/models/ajouter_contact.py
@@ -48,8 +48,7 @@ class facture(models.Model):
                         variable_ = item_ids[0].id
                         if variable_:
                             self.x_contact = variable_
-                else:
-                    raise ValidationError(_("eror "))
+        
 
 
 
