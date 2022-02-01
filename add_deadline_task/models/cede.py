94c73b6758b65e594a6387a593a74f6e5182f656
abdelghani-khalidi <51909290+abdelghani-khalidi@users.noreply.github.com>
2022-02-01 17:10:31 +0100

    Update cede.py

--- add_deadline_task/models/cede.py
+++ add_deadline_task/models/cede.py
@@ -11,3 +11,21 @@ class task_task_limite(models.Model):
     date_deadline = fields.Datetime(string='Deadline', index=True, copy=False, tracking=True)
     ticket_id = fields.Many2one('helpdesk.ticket', string="Ticket")
 
+class task_task_ticket_button(models.Model):
+    _inherit = 'planning.slot'
+
+    def open_task(self):
+     if self.task_id:
+        return {
+            'type': 'ir.actions.act_url',
+            'url': 'https://dkgroup.odoo.com/web#cids=36&id=%s&model=project.task' % (self.task_id.id),
+            'target': 'new',
+        }
+    
+    def open_ticket(self):
+     if self.task_id.ticket_id:
+        return {
+            'type': 'ir.actions.act_url',
+            'url': 'https://erp.dkgroup.fr/web#cids=36&id=%s&model=helpdesk.ticket&menu_id=' % (self.task_id.ticket_id.id),
+            'target': 'new',
+        }
