cb6ec39fc938106985be449830660a0414961441
Asmaa-khallout <asmaa.khallout@dkgroup.fr>
2022-02-22 11:39:21 +0100

    ADD custom CRM

---
+++ custom_crm/__manifest__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+# Part of BrowseInfo. See LICENSE file for full copyright and licensing details.
+
+{
+    'name' : "Custom CRM Dk group",
+    'version' : "13.0.0.0",
+    'description' : """
+        
+        
+
+     """,
+    'author' : "Asmaa Khallout",
+    'website'  : "https://dkgroup.fr",
+    'depends': ['crm'],
+
+    'data'     : ['views/crm_lead_views_inherit.xml'],
+    'installable' : True,
+    'application' :  True,
+    
+}
