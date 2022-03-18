ae90a9f686adc4baf2800e021e93cd83ac80274c
Asmaa-khallout <asmaa.khallout@dkgroup.fr>
2022-03-18 12:25:43 +0100

    Check document access

---
+++ dkgroup_upgrade/__manifest__.py
@@ -0,0 +1,17 @@
+# Copyright 2016-2017 Akretion (http://www.akretion.com)
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+{
+    'name': 'Upgrade Version Dk Group',
+    'version': '12.0.1.0.0',
+    'summary': 'Upgrade Version Dk Group'
+    ' categories',
+    'author': 'Asmaa Khallout',
+    'website': 'https://DKGROUP.FR',
+    'license': 'AGPL-3',
+    'category': 'Generic Modules',
+    'depends': ['base'],
+    'data': [],
+    'application': True,
+    'installable': True,
+}
