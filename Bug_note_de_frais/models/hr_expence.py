2c5b3b4102c3dbd3e977bbabfe0534ca8ef77d2d
aichagoup <aicha.bouchouk@dkgroup.fr>
2022-04-05 15:35:44 +0000

    update module

--- Bug_note_de_frais/models/hr_expence.py
+++ Bug_note_de_frais/models/hr_expence.py
@@ -14,7 +14,16 @@ class hr_expense(models.Model):
 
     def new_button(self):
         test = self._create_sheet_from_expenses()
+        test.action_submit_sheet()
         _logger.info('fffffffffffffffffffff %s',test)
+        return {
+            'name': _('New Expense Report'),
+            'type': 'ir.actions.act_window',
+            'view_mode': 'form',
+            'res_model': 'hr.expense.sheet',
+            'target': 'current',
+            'res_id': test.id,
+        }
 
 
 
