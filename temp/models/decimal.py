a89eb5ab47df09388683b2342529064e01f5da5f
abdelghani-khalidi <51909290+abdelghani-khalidi@users.noreply.github.com>
2022-04-18 16:59:54 +0200

    Update decimal.py

--- temp/models/decimal.py
+++ temp/models/decimal.py
@@ -22,27 +22,3 @@ class DecimalPprecision(models.Model):
 
 
 
-class ResPartner2(models.Model):
-    _inherit = 'res.partner'
-
-    def update_next_action(self, options=False):
-        """Updates the next_action_date of the right account move lines"""
-        next_action_date = options.get('next_action_date') and options['next_action_date'][0:10] or False
-        next_action_date_done = False
-        today = date.today()
-        fups = self._compute_followup_lines()
-        for partner in self:
-            if options['action'] == 'done':
-                next_action_date_done = datetime.strftime(partner.followup_level._get_next_date(), DEFAULT_SERVER_DATE_FORMAT)
-            partner.payment_next_action_date = (not next_action_date or options['action'] == 'done') and next_action_date_done or next_action_date
-            if options['action'] in ('done', 'later'):
-                msg = _('Next Reminder Date set to %s') % format_date(self.env, partner.payment_next_action_date)
-                partner.message_post(body=msg)
-            if options['action'] == 'done':
-                for aml in partner.unreconciled_aml_ids:
-                    index = None
-                    followup_date = fups[index][0]
-                    next_level = fups[index][1]
-                    is_overdue = followup_date >= aml.date_maturity if aml.date_maturity else followup_date >= aml.date
-                    if is_overdue:
-                        aml.write({'followup_line_id': next_level, 'followup_date': today})
