7180606cce57d8cb00d2c66108928c7dd9ca0e42
abdelghani-khalidi <51909290+abdelghani-khalidi@users.noreply.github.com>
2022-04-22 16:53:43 +0200

    Add files via upload

---
+++ block_followup_automatic/__manifest__.py
@@ -0,0 +1,17 @@
+# Copyright 2016-2017 Akretion (http://www.akretion.com)
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+{
+    'name': 'block_followup_automatic_for_partner',
+    'version': '13.0.1.0.0',
+    'summary': 'block_followup_automatic_for_partner'
+    ' State',
+    'author': 'Abdelghani KHALIDI',
+    'website': 'https://dkgroup.fr',
+    'price': 5000.9,
+    'currency': 'EUR',
+    'license': 'OPL-1',
+    'category': 'Generic Modules',
+    'depends': ['base','account_followup'],
+    'installable': True,
+}
---
+++ block_followup_automatic/models/state.py
@@ -0,0 +1,26 @@
+import logging
+from odoo import api, fields, models, _
+from odoo.tools.misc import format_date
+from odoo.osv import expression
+from datetime import date, datetime, timedelta
+from odoo.tools import DEFAULT_SERVER_DATE_FORMAT
+from odoo.exceptions import UserError
+
+_logger = logging.getLogger(__name__)
+
+
+class ResPartner(models.Model):
+    _inherit = 'res.partner'
+    is_blocked = fields.Boolean(string='Bloquer les relances automatiques', default=False)
+
+    def _cron_execute_followup_company(self):
+        followup_data = self._query_followup_level(all_partners=True)
+        in_need_of_action = self.env['res.partner'].browse([d['partner_id'] for d in followup_data.values() if d['followup_status'] == 'in_need_of_action' and d['is_blocked'] == False ])
+        in_need_of_action_auto = in_need_of_action.filtered(lambda p: p.followup_level.auto_execute)
+        for partner in in_need_of_action_auto:
+            try:
+                partner._execute_followup_partner()
+            except UserError as e:
+                # followup may raise exception due to configuration issues
+                # i.e. partner missing email
+                _logger.exception(e)
