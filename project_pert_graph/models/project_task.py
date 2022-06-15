ef8cdc6806ae2b5dc690e86cd5d3fba6188d5f29
abdelghani-khalidi <51909290+abdelghani-khalidi@users.noreply.github.com>
2022-06-15 11:17:24 +0200

    Update project_task.py

--- project_pert_graph/models/project_task.py
+++ project_pert_graph/models/project_task.py
@@ -174,6 +174,7 @@ class ProjectTask(models.Model):
                 task.planned_date_end = task.date_deadline
             else:
                 hours = task.planned_hours or 1.0
+                task.planned_date_end = task.planned_date_begin + fields.Datetime.now()
 #                 task.planned_date_end = (task.planned_date_begin + fields.Datetime.now()) + timedelta(hours=int(hours))
 
             if hasattr(task, 'planns'):
